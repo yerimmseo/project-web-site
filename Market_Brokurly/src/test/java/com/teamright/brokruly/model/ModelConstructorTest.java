8baaa97292bcbe50fd5f6c3e4ede0d41929343a4
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-08-13 09:35:13 +0900

    모델 클래스 생성자 테스트 진행

--- Market_Brokurly/src/test/java/com/teamright/brokruly/model/ModelConstructorTest.java
+++ Market_Brokurly/src/test/java/com/teamright/brokruly/model/ModelConstructorTest.java
@@ -1,8 +1,13 @@
 package com.teamright.brokruly.model;
 
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+/*
+ * 모델 생성자에 대한 테스트 진행
+ * */
 public class ModelConstructorTest {
 
 	Logger log = Logger.getLogger(this.getClass());
@@ -40,61 +45,73 @@ public class ModelConstructorTest {
 
 	@Test
 	public void TestDeliveryVO() {
-		
+		DeliveryVO delivery = new DeliveryVO(113, "배송완료", "서울시 강남", "customer4", 123);
+		log.info(delivery);
 	}
 	
 	@Test
 	public void TestEventVO() {
-		
+		EventVO event = new EventVO(31, "과일항목 50%할인");
+		log.info(event);
 	}
 	
 	@Test
 	public void TestMainCategoryVO() {
-		
+		MainCategoryVO mCate = new MainCategoryVO(12, "고구마·감자");
+		log.info(mCate);
 	}
 	
 	@Test
 	public void TestManagerVO() {
-		
+		ManagerVO manager = new ManagerVO("brokurly", "1234", "brokurly");
 	}
 	
 	@Test
 	public void TestMileageHistoryVO() {
-		
+		MileageHistoryVO mHistory = new MileageHistoryVO(5000, "+5000");
+		log.info(mHistory);
 	}
 	
 	@Test
 	public void TestOrderVO() {
-		
+		OrderVO order = new OrderVO(1322);
+		log.info(order);
 	}
 	
 	@Test
 	public void TestProductBbsVO() {
-		
+		ProductBbsVO proBbs = new ProductBbsVO(1234);
+		log.info(proBbs);
 	}
 	
 	@Test
 	public void TestProductVO() {
-		
+		ProductVO product = new ProductVO(123, "친환경 대파 250g", "국내산");
+		log.info(product);
 	}
 	
 	@Test
 	public void TestReplyVO() {
-		
+		ReplyVO reply = new ReplyVO(1234, "답변완료에 대한 내용입력");
+		log.info(reply);
 	}
 	
 	@Test
 	public void TestSalesVO() {
-		
+		Date date = new Date();
+		SalesVO sales = new SalesVO(44, 2, date);
+		log.info(sales);
 	}
 	
 	@Test
 	public void TestStockVO() {
-		
+		StockVO stock = new StockVO(1112, 3000);
+		log.info(stock);
 	}
 	
 	@Test
 	public void TestSubCategoryVO() {
-		
+		SubCategoryVO sCate = new SubCategoryVO(22, "견과류");
+		log.info(sCate);
 	}
 }
