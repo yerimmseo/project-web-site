7825249d975c7eb895747ef85ed5d6f5b0161f8f
yerimmseo <yerim7386@naver.com>
2021-08-20 23:15:58 +0900

    로그인 전용 js 생성

---
+++ Market_Brokurly/src/main/webapp/resources/js/signin.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+$(document).ready(function(e) {
+	$('#login_btn').click(function(){
+
+		$.ajax({
+			url: "/brokurly/customer/login",
+			type: "GET",
+			data: {
+				"customer_id": $('#customer_id').val(),
+				"customer_pw": $('#customer_pw').val()
+			},
+			success:function(data) {
+				if (data == '') {
+					alert("아이디 또는 패스워드 오류");
+					return;
+				} else {
+					alert(data + "님 환영합니다.");
+					$('#login_form').submit();
+				}
+			},
+			error:function() {
+				alert("서버오류");
+			}
+		});
+	}); 
+
+	$('#signup').click(function() {
+		$('#signupForm').submit();
+	});
+
+	$('#findid').click(function() {
+		$('#findidForm').submit();
+	});
+
+	$('#findpw').click(function() {
+		$('#findpwForm').submit();
+	});
+
+});
+
+
+
