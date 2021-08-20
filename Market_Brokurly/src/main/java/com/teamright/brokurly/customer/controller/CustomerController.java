62902cd0c04c326301615b8b554d1fb16af5354a
yerimmseo <yerim7386@naver.com>
2021-08-20 22:09:16 +0900

    customer controller 세분화 진행중

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/controller/CustomerController.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/controller/CustomerController.java
@@ -1,40 +1,41 @@
 package com.teamright.brokurly.customer.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.teamright.brokurly.customer.mapper.CustomerMapper;
-import com.teamright.brokurly.model.CustomerVO;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
-	@Autowired
-	CustomerMapper customerMapper;
-	
+
 	@GetMapping("/signup")
 	public void signup() {}
-	
+
 	@GetMapping("/signin")
 	public void signin() {}
-	
+
 	@GetMapping("/findid")
 	public void findid() {}
-	
+
 	@GetMapping("/findpw")
 	public void findpw() {}
 	
-	@GetMapping("/joinok")
-	public void joinok() {}
-	
-	@RequestMapping("/mypage")
-	public void myPage(Model model) {
-		CustomerVO customer = customerMapper.getCustomerInfo("mongsoung");
-		
-		model.addAttribute("customer", customer);
-	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
