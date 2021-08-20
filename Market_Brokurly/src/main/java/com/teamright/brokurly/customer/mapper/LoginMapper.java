d7adfc171a3fee7de759c4e2e743d7bf934c9a7d
yerimmseo <yerim7386@naver.com>
2021-08-20 22:13:39 +0900

    로그인 전용 매퍼 생성

---
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/mapper/LoginMapper.java
@@ -0,0 +1,12 @@
+package com.teamright.brokurly.customer.mapper;
+
+public interface LoginMapper {
+	   // 아이디 체크
+	   public String idCheck(String customer_id);
+	   
+	   // 비밀번호 체크
+	   public String pwCheck(String customer_id);
+	   
+	   // 이름 가져오기
+	   public String getName(String customer_id);
+}
