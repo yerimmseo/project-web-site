0d4de51e15e337d0ad909b8339b35fdab362a925
yerimmseo <yerim7386@naver.com>
2021-08-28 23:48:36 +0900

    고객 정보 데이터를 가공할 서비스 인터페이스 생성

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/FindService.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/FindService.java
@@ -1,6 +1,16 @@
 package com.teamright.brokurly.customer.service;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface FindService {
 	// 아이디 찾기(가져오기)
-	public String getID(String customer_name, String customer_email);
+	public String getID(@Param("customer_name")String customer_name, 
+						@Param("customer_email")String customer_email);
+	// 비밀번호 찾기(가져오기)
+	public String getPW(@Param("customer_name")String customer_name,
+						@Param("customer_id")String customer_id,
+						@Param("customer_email")String customer_email);
+	// 비밀번호 변경
+	public void changePW(@Param("customer_pw")String customer_pw,
+			 			 @Param("customer_id")String cusotmer_id);
 }
--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/FindServiceImpl.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/FindServiceImpl.java
@@ -1,5 +1,6 @@
 package com.teamright.brokurly.customer.service;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,24 @@ public class FindServiceImpl implements FindService {
 	FindMapper findMapper;
 
 	@Override
-	public String getID(String customer_name, String customer_email) {
-		return findMapper.getID(customer_name, customer_email);
+	public String getID(@Param("customer_name")String customer_name, 
+						@Param("customer_email")String customer_email) {
+		String result = findMapper.getID(customer_name, customer_email);
+		return result;
+	}
+
+	@Override
+	public String getPW(@Param("customer_name")String customer_name,
+						@Param("customer_id")String customer_id,
+						@Param("customer_email")String customer_email) {
+		String result = findMapper.getPW(customer_name, customer_id, customer_email);
+		
+		return result;
 	}
 	
+	// 비밀번호 변경
+	public void changePW(@Param("customer_pw")String customer_pw,
+			 			 @Param("customer_id")String cusotmer_id) {
+		findMapper.changePW(customer_pw, cusotmer_id);
+	}
 }
