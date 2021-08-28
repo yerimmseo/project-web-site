fc51bc919de687df2baa28a6026ef8a88dde026e
yerimmseo <yerim7386@naver.com>
2021-08-28 23:47:31 +0900

    고객 정보 찾기 컨트롤러 생성

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/controller/FindController.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/controller/FindController.java
@@ -1,11 +1,16 @@
 package com.teamright.brokurly.customer.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.teamright.brokurly.customer.service.FindService;
+import com.teamright.brokurly.customer.service.SignInService;
 
 @Controller
 @RequestMapping("/customer")
@@ -14,9 +19,35 @@ public class FindController {
 	@Autowired
 	FindService findService;
 	
-//	@ResponseBody
-//	public String findID() {
-//		
-//	}
-
+	@Autowired
+	private SignInService loginService;
+	
+	@RequestMapping(value="/getId", method=RequestMethod.GET, produces="application/text; charset=utf8")
+	@ResponseBody
+	public String findID(HttpServletRequest request) {
+		String result = findService.getID(
+				request.getParameter("customer_name"), request.getParameter("customer_email"));
+		
+		return result;
+	}
+	
+	@RequestMapping(value="/getPw", method=RequestMethod.GET, produces="application/text; charset=utf8")
+	@ResponseBody
+	public String findPW(HttpSession session, HttpServletRequest request) {
+		String result = findService.getPW(
+				request.getParameter("customer_name"), request.getParameter("customer_id"), request.getParameter("customer_email"));
+		session.setAttribute("customer_id", request.getParameter("customer_id"));
+		return result;
+	}
+	
+	@RequestMapping(value="/changePw", method=RequestMethod.GET, produces="application/text; charset=utf8")
+	@ResponseBody
+	public String changePW(HttpSession session, HttpServletRequest request) throws Exception {
+		String customer_id = (String) session.getAttribute("customer_id");
+		String customer_pw = loginService.getSHA512(request.getParameter("customer_pw"));
+		
+		findService.changePW(customer_pw, customer_id);
+		
+		return null;
+	}
 }
