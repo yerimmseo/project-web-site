04652f030f1241b9c8aafe59007ce6781713e1c1
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-09-01 14:09:08 +0900

    header.jsp에서 필요한 고객정보에 관한 함수추가

--- Market_Brokurly/src/main/webapp/resources/js/style/header.js
+++ Market_Brokurly/src/main/webapp/resources/js/style/header.js
@@ -39,3 +39,54 @@ function Mclick_service() {
         check = true;
     }, 4000);
 }
+
+/* ----------------------------- 고객명 하위메뉴 클릭시 나타남 ---------------------------- */
+function Mclick_customer() {
+	const customer_menu = document.querySelector('#customer_menu');
+	
+	
+	if(check){
+		customer_menu.style.visibility="visible";
+		check = false;
+	} else {
+		customer_menu.style.visibility="hidden";
+		check = true;
+	}
+	setTimeout(function() {
+		customer_menu.style.visibility="hidden";
+		check = true;
+	}, 4000);
+}
+
+/* ------------------------------- 검색시 해당 품목 가져오기 ----------------------------------- */
+$('#search_a').click(function(){
+	
+	$.ajax({
+		url: "/brokurly/products/search",
+		type: "GET",
+		data: {
+			"search_name": $('#search').val()
+		},
+		success:function(){
+			alert("성공")
+		},
+		error:function(){
+			alert("에러");
+		}
+	});
+});	
+
+
+$('#logout').click(function(){
+	$.ajax({
+		url: "/brokurly/customer/logout",
+		type: "GET",
+		success:function(data){
+			alert("로그아웃 성공");
+			$('#logoutForm').submit();
+		},
+		error:function(){
+			alert("에러");
+		}
+	});
+});
