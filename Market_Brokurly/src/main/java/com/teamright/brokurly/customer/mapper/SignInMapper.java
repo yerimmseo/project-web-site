0889dc1578472d1e34fa94efc230903e10f407d9
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-09-01 14:19:41 +0900

    주소를 가져오는 getAddress와 등급을 가져오는 getGrade함수 추가

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/mapper/SignInMapper.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/mapper/SignInMapper.java
@@ -1,4 +1,9 @@
+  
 package com.teamright.brokurly.customer.mapper;
+
+import java.util.List;
+
+import com.teamright.brokurly.model.AddressVO;
 
 public interface SignInMapper {
 	// 아이디 체크
@@ -7,4 +12,8 @@ public interface SignInMapper {
 	public String pwCheck(String customer_id);
 	// 이름 가져오기
 	public String getName(String customer_id);
+	// 주소 가져오기
+	public List<AddressVO> getAddress(String customer_id);
+	// 등급 가져오기
+	public String getGrade(String customer_id);
 }
