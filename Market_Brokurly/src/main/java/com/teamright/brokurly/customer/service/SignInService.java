89b4418aba0d8fa5c45d6a7108a634b6ec736a94
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-09-01 14:20:20 +0900

    getAddress와 getGrade 함수 추가

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/SignInService.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/SignInService.java
@@ -1,5 +1,9 @@
 package com.teamright.brokurly.customer.service;
 
+import java.util.List;
+
+import com.teamright.brokurly.model.AddressVO;
+
 public interface SignInService {
 	// 비밀번호 암호화
 	public String getSHA512(String customer_pw) throws Exception;
@@ -9,4 +13,8 @@ public interface SignInService {
 	public String pwCheck(String customer_id);
 	// 이름 가져오기
 	public String getName(String customer_id);
+	// 주소 가져오기
+	public List<AddressVO> getAddress(String customer_id);
+	// 등급 가져오기
+	public String getGrade(String customer_id);
 }
