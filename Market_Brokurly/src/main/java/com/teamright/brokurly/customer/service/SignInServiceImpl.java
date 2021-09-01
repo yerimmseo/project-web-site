805227e4679ed7efcc8781a42872255f1e276303
LeeSeungUn95 <82787224+LeeSeungUn95@users.noreply.github.com>
2021-09-01 14:20:38 +0900

    getAddress와 getGrade 함수 추가

--- Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/SignInServiceImpl.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/customer/service/SignInServiceImpl.java
@@ -2,11 +2,13 @@ package com.teamright.brokurly.customer.service;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.teamright.brokurly.customer.mapper.SignInMapper;
+import com.teamright.brokurly.model.AddressVO;
 
 @Service
 public class SignInServiceImpl implements SignInService {
@@ -37,6 +39,18 @@ public class SignInServiceImpl implements SignInService {
 	@Override
 	public String getName(String customer_id) {
 		return loginMapper.getName(customer_id);
+	}
+
+	@Override
+	public List<AddressVO> getAddress(String customer_id) {
+		List<AddressVO> address_list = loginMapper.getAddress(customer_id);
+		return address_list;
+	}
+
+	@Override
+	public String getGrade(String customer_id) {
+		String result = loginMapper.getGrade(customer_id);
+		return result;
 	}
 
 }
